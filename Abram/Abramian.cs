ec9ac29ce6bb8eebb482ea2d9d0e267904f5860d
Arthur <Warriorroq@gmail.com>
2021-09-10 13:43:22 +0300

    For 1 - 2
    ~3 min

--- Abram/Abramian.cs
+++ Abram/Abramian.cs
@@ -7,16 +7,19 @@ namespace AbramianHomeWork.Abram
         private BooleanTask _boolean;
         private If _if;
         private Case _case;
+        private For _for;
         public Abramian()
         {
             _integer = new IntegerTask();
             _boolean = new BooleanTask();
             _if = new If();
             _case = new Case();
+            _for = new For();
         }
         public void Run()
         {
-            Console.WriteLine(_case.Case20(25, 7));
+            foreach (var i in _for.For2(2,7))
+                Console.WriteLine(i);
         }
     }
 }
---
+++ Abram/For.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbramianHomeWork.Abram
+{
+    public class For
+    {
+        public IEnumerable For1(int n, int k)
+            => Enumerable.Range(0, n).Select(n => k);
+        public List<int> For2(int a, int b)
+        {
+            var nums = Enumerable.Range(a, b - 1).Select(a => a).ToList();
+            nums.Add(nums.Count);
+            return nums;
+        }
+    }
+}
